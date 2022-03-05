bc6c21d63ef196514135707410a06d16b3f27e8c
Lai <lai@caju.com.br>
2022-03-04 22:35:45 -0300

    config vercel server

--- remix.config.js
+++ remix.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   serverBuildTarget: "vercel",
   ignoredRouteFiles: [".*"],
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
