5fed132d6991e98533a78124417f62be64cfbbde
Lai <lai@caju.com.br>
2022-03-04 22:40:40 -0300

    change vercel config

--- server.js
+++ server.js
@@ -2,4 +2,3 @@ import { createRequestHandler } from "@remix-run/vercel";
 import * as build from "@remix-run/dev/server-build";
 
 export default createRequestHandler({ build, mode: process.env.NODE_ENV });
-
