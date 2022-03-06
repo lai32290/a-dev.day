49669d2bc29ad93d2613d5f54de9cfd1cd020bd1
Lai <lai@caju.com.br>
2022-03-06 08:56:02 -0300

    change post path

---
+++ api/index.js
@@ -0,0 +1,582 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __esm = (fn, res) => function __init() {
+  return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])(fn = 0)), res;
+};
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __reExport = (target, module2, copyDefault, desc) => {
+  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
+    for (let key of __getOwnPropNames(module2))
+      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
+        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toESM = (module2, isNodeMode) => {
+  return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", !isNodeMode && module2 && module2.__esModule ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
+};
+var __toCommonJS = /* @__PURE__ */ ((cache) => {
+  return (module2, temp) => {
+    return cache && cache.get(module2) || (temp = __reExport(__markAsModule({}), module2, 1), cache && cache.set(module2, temp), temp);
+  };
+})(typeof WeakMap !== "undefined" ? /* @__PURE__ */ new WeakMap() : 0);
+
+// node_modules/@remix-run/dev/compiler/shims/react.ts
+var React;
+var init_react = __esm({
+  "node_modules/@remix-run/dev/compiler/shims/react.ts"() {
+    React = __toESM(require("react"));
+  }
+});
+
+// node_modules/remix/index.js
+var require_remix = __commonJS({
+  "node_modules/remix/index.js"(exports) {
+    "use strict";
+    init_react();
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var node = require("@remix-run/node");
+    Object.defineProperty(exports, "createFileSessionStorage", {
+      enumerable: true,
+      get: function() {
+        return node.createFileSessionStorage;
+      }
+    });
+    Object.defineProperty(exports, "unstable_createFileUploadHandler", {
+      enumerable: true,
+      get: function() {
+        return node.unstable_createFileUploadHandler;
+      }
+    });
+    Object.defineProperty(exports, "unstable_createMemoryUploadHandler", {
+      enumerable: true,
+      get: function() {
+        return node.unstable_createMemoryUploadHandler;
+      }
+    });
+    Object.defineProperty(exports, "unstable_parseMultipartFormData", {
+      enumerable: true,
+      get: function() {
+        return node.unstable_parseMultipartFormData;
+      }
+    });
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var serverRuntime = require("@remix-run/server-runtime");
+    Object.defineProperty(exports, "createCookie", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.createCookie;
+      }
+    });
+    Object.defineProperty(exports, "createCookieSessionStorage", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.createCookieSessionStorage;
+      }
+    });
+    Object.defineProperty(exports, "createMemorySessionStorage", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.createMemorySessionStorage;
+      }
+    });
+    Object.defineProperty(exports, "createSession", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.createSession;
+      }
+    });
+    Object.defineProperty(exports, "createSessionStorage", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.createSessionStorage;
+      }
+    });
+    Object.defineProperty(exports, "isCookie", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.isCookie;
+      }
+    });
+    Object.defineProperty(exports, "isSession", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.isSession;
+      }
+    });
+    Object.defineProperty(exports, "json", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.json;
+      }
+    });
+    Object.defineProperty(exports, "redirect", {
+      enumerable: true,
+      get: function() {
+        return serverRuntime.redirect;
+      }
+    });
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var react = require("@remix-run/react");
+    Object.defineProperty(exports, "Form", {
+      enumerable: true,
+      get: function() {
+        return react.Form;
+      }
+    });
+    Object.defineProperty(exports, "Link", {
+      enumerable: true,
+      get: function() {
+        return react.Link;
+      }
+    });
+    Object.defineProperty(exports, "Links", {
+      enumerable: true,
+      get: function() {
+        return react.Links;
+      }
+    });
+    Object.defineProperty(exports, "LiveReload", {
+      enumerable: true,
+      get: function() {
+        return react.LiveReload;
+      }
+    });
+    Object.defineProperty(exports, "Meta", {
+      enumerable: true,
+      get: function() {
+        return react.Meta;
+      }
+    });
+    Object.defineProperty(exports, "NavLink", {
+      enumerable: true,
+      get: function() {
+        return react.NavLink;
+      }
+    });
+    Object.defineProperty(exports, "Outlet", {
+      enumerable: true,
+      get: function() {
+        return react.Outlet;
+      }
+    });
+    Object.defineProperty(exports, "PrefetchPageLinks", {
+      enumerable: true,
+      get: function() {
+        return react.PrefetchPageLinks;
+      }
+    });
+    Object.defineProperty(exports, "RemixBrowser", {
+      enumerable: true,
+      get: function() {
+        return react.RemixBrowser;
+      }
+    });
+    Object.defineProperty(exports, "RemixServer", {
+      enumerable: true,
+      get: function() {
+        return react.RemixServer;
+      }
+    });
+    Object.defineProperty(exports, "Scripts", {
+      enumerable: true,
+      get: function() {
+        return react.Scripts;
+      }
+    });
+    Object.defineProperty(exports, "ScrollRestoration", {
+      enumerable: true,
+      get: function() {
+        return react.ScrollRestoration;
+      }
+    });
+    Object.defineProperty(exports, "useActionData", {
+      enumerable: true,
+      get: function() {
+        return react.useActionData;
+      }
+    });
+    Object.defineProperty(exports, "useBeforeUnload", {
+      enumerable: true,
+      get: function() {
+        return react.useBeforeUnload;
+      }
+    });
+    Object.defineProperty(exports, "useCatch", {
+      enumerable: true,
+      get: function() {
+        return react.useCatch;
+      }
+    });
+    Object.defineProperty(exports, "useFetcher", {
+      enumerable: true,
+      get: function() {
+        return react.useFetcher;
+      }
+    });
+    Object.defineProperty(exports, "useFetchers", {
+      enumerable: true,
+      get: function() {
+        return react.useFetchers;
+      }
+    });
+    Object.defineProperty(exports, "useFormAction", {
+      enumerable: true,
+      get: function() {
+        return react.useFormAction;
+      }
+    });
+    Object.defineProperty(exports, "useHref", {
+      enumerable: true,
+      get: function() {
+        return react.useHref;
+      }
+    });
+    Object.defineProperty(exports, "useLoaderData", {
+      enumerable: true,
+      get: function() {
+        return react.useLoaderData;
+      }
+    });
+    Object.defineProperty(exports, "useLocation", {
+      enumerable: true,
+      get: function() {
+        return react.useLocation;
+      }
+    });
+    Object.defineProperty(exports, "useMatches", {
+      enumerable: true,
+      get: function() {
+        return react.useMatches;
+      }
+    });
+    Object.defineProperty(exports, "useNavigate", {
+      enumerable: true,
+      get: function() {
+        return react.useNavigate;
+      }
+    });
+    Object.defineProperty(exports, "useNavigationType", {
+      enumerable: true,
+      get: function() {
+        return react.useNavigationType;
+      }
+    });
+    Object.defineProperty(exports, "useOutlet", {
+      enumerable: true,
+      get: function() {
+        return react.useOutlet;
+      }
+    });
+    Object.defineProperty(exports, "useOutletContext", {
+      enumerable: true,
+      get: function() {
+        return react.useOutletContext;
+      }
+    });
+    Object.defineProperty(exports, "useParams", {
+      enumerable: true,
+      get: function() {
+        return react.useParams;
+      }
+    });
+    Object.defineProperty(exports, "useResolvedPath", {
+      enumerable: true,
+      get: function() {
+        return react.useResolvedPath;
+      }
+    });
+    Object.defineProperty(exports, "useSearchParams", {
+      enumerable: true,
+      get: function() {
+        return react.useSearchParams;
+      }
+    });
+    Object.defineProperty(exports, "useSubmit", {
+      enumerable: true,
+      get: function() {
+        return react.useSubmit;
+      }
+    });
+    Object.defineProperty(exports, "useTransition", {
+      enumerable: true,
+      get: function() {
+        return react.useTransition;
+      }
+    });
+  }
+});
+
+// server.js
+var server_exports = {};
+__export(server_exports, {
+  default: () => server_default
+});
+init_react();
+var import_vercel = require("@remix-run/vercel");
+
+// server-entry-module:@remix-run/dev/server-build
+var server_build_exports = {};
+__export(server_build_exports, {
+  assets: () => assets_manifest_default,
+  entry: () => entry,
+  routes: () => routes
+});
+init_react();
+
+// app/entry.server.tsx
+var entry_server_exports = {};
+__export(entry_server_exports, {
+  default: () => handleRequest
+});
+init_react();
+var import_server = require("react-dom/server");
+var import_remix = __toESM(require_remix());
+function handleRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  const markup = (0, import_server.renderToString)(/* @__PURE__ */ React.createElement(import_remix.RemixServer, {
+    context: remixContext,
+    url: request.url
+  }));
+  responseHeaders.set("Content-Type", "text/html");
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders
+  });
+}
+
+// route:/Users/lai/Documents/Projects/Personal/a-dev.day/app/root.tsx
+var root_exports = {};
+__export(root_exports, {
+  default: () => App,
+  links: () => links,
+  meta: () => meta
+});
+init_react();
+var import_remix2 = __toESM(require_remix());
+
+// app/styles/app.css
+var app_default = "/build/_assets/app-NGENMFQW.css";
+
+// route:/Users/lai/Documents/Projects/Personal/a-dev.day/app/root.tsx
+function links() {
+  return [
+    { rel: "stylesheet", href: app_default }
+  ];
+}
+var meta = () => {
+  return { title: "New Remix App" };
+};
+function App() {
+  return /* @__PURE__ */ React.createElement("html", {
+    lang: "en"
+  }, /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement("meta", {
+    charSet: "utf-8"
+  }), /* @__PURE__ */ React.createElement("meta", {
+    name: "viewport",
+    content: "width=device-width,initial-scale=1"
+  }), /* @__PURE__ */ React.createElement(import_remix2.Meta, null), /* @__PURE__ */ React.createElement(import_remix2.Links, null)), /* @__PURE__ */ React.createElement("body", {
+    className: "h-screen"
+  }, /* @__PURE__ */ React.createElement(import_remix2.Outlet, null), /* @__PURE__ */ React.createElement(import_remix2.ScrollRestoration, null), /* @__PURE__ */ React.createElement(import_remix2.Scripts, null), /* @__PURE__ */ React.createElement(import_remix2.LiveReload, null)));
+}
+
+// route:/Users/lai/Documents/Projects/Personal/a-dev.day/app/routes/$slug.tsx
+var slug_exports = {};
+__export(slug_exports, {
+  ErrorBoundary: () => ErrorBoundary,
+  default: () => PostSlug,
+  loader: () => loader
+});
+init_react();
+var import_remix3 = __toESM(require_remix());
+var import_tiny_invariant2 = __toESM(require("tiny-invariant"));
+
+// app/post.ts
+init_react();
+var import_path = __toESM(require("path"));
+var import_tiny_invariant = __toESM(require("tiny-invariant"));
+var import_marked = require("marked");
+var import_promises = __toESM(require("fs/promises"));
+var import_front_matter = __toESM(require("front-matter"));
+var postsPath = false ? import_path.default.resolve(__dirname, "../posts") : import_path.default.resolve(__dirname, "../../../../posts");
+function isValidPostAttributes(attributes) {
+  var _a;
+  return (_a = attributes == null ? void 0 : attributes.meta) == null ? void 0 : _a.title;
+}
+async function getPosts() {
+  const dir = await import_promises.default.readdir(postsPath);
+  return Promise.all(dir.map(async (filename) => {
+    const file = await import_promises.default.readFile(import_path.default.join(postsPath, filename));
+    const { attributes } = (0, import_front_matter.default)(file.toString());
+    (0, import_tiny_invariant.default)(isValidPostAttributes(attributes), `${filename} has bad meta data!`);
+    return {
+      slug: filename.replace(/\.mdx?$/, ""),
+      title: attributes.meta.title
+    };
+  }));
+}
+async function getPost(slug) {
+  const filepath = import_path.default.join(postsPath, `${slug}.mdx`);
+  const file = await import_promises.default.readFile(filepath);
+  const { attributes, body } = (0, import_front_matter.default)(file.toString());
+  (0, import_tiny_invariant.default)(isValidPostAttributes(attributes), `Post ${filepath} is missing attributes`);
+  const html = (0, import_marked.marked)(body);
+  return {
+    slug,
+    title: attributes.meta.title,
+    html
+  };
+}
+
+// route:/Users/lai/Documents/Projects/Personal/a-dev.day/app/routes/$slug.tsx
+async function loader({ params }) {
+  (0, import_tiny_invariant2.default)(params.slug, "expected params.slug");
+  return getPost(params.slug);
+}
+function PostSlug() {
+  const post = (0, import_remix3.useLoaderData)();
+  return /* @__PURE__ */ React.createElement("main", null, /* @__PURE__ */ React.createElement("h1", null, post.title), /* @__PURE__ */ React.createElement("div", {
+    dangerouslySetInnerHTML: { __html: post.html }
+  }));
+}
+function ErrorBoundary({ error }) {
+  console.error(error);
+  return /* @__PURE__ */ React.createElement("div", null, "jfids");
+}
+
+// route:/Users/lai/Documents/Projects/Personal/a-dev.day/app/routes/index.tsx
+var routes_exports = {};
+__export(routes_exports, {
+  default: () => Index,
+  loader: () => loader2
+});
+init_react();
+var import_remix4 = __toESM(require_remix());
+
+// app/components/Navbar/index.tsx
+init_react();
+function Navbar() {
+  return /* @__PURE__ */ React.createElement("header", {
+    className: "px-10 py-4 bg-gray-700 text-slate-50 text-4xl"
+  }, "a dev.day");
+}
+
+// app/components/Footer/index.tsx
+init_react();
+function Footer() {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "px-10 py-4 bg-gray-700 text-slate-50 flex justify-between"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "text-3xl"
+  }, "a dev.day"), /* @__PURE__ */ React.createElement("nav", {
+    className: "flex items-center"
+  }, /* @__PURE__ */ React.createElement("ul", {
+    className: "flex justify-between"
+  }, /* @__PURE__ */ React.createElement("li", {
+    className: "pl-4"
+  }, /* @__PURE__ */ React.createElement("a", {
+    href: "#"
+  }, "Github")), /* @__PURE__ */ React.createElement("li", {
+    className: "pl-4"
+  }, /* @__PURE__ */ React.createElement("a", {
+    href: "#"
+  }, "Linkedin")))));
+}
+
+// route:/Users/lai/Documents/Projects/Personal/a-dev.day/app/routes/index.tsx
+async function loader2() {
+  return getPosts();
+}
+function Index() {
+  const posts = (0, import_remix4.useLoaderData)();
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "flex flex-col h-screen"
+  }, /* @__PURE__ */ React.createElement(Navbar, null), /* @__PURE__ */ React.createElement("div", {
+    className: "bg-gray-600 text-slate-50 px-14 py-7 grow"
+  }, /* @__PURE__ */ React.createElement("ul", null, posts.map((post) => /* @__PURE__ */ React.createElement("li", {
+    key: post.slug,
+    className: "flex justify-between"
+  }, /* @__PURE__ */ React.createElement(import_remix4.Link, {
+    to: post.slug,
+    className: "text-2xl text-sky-200 underline"
+  }, post.title), /* @__PURE__ */ React.createElement("span", null, "4 March, 2022"))))), /* @__PURE__ */ React.createElement(Footer, null));
+}
+
+// server-assets-manifest:@remix-run/dev/assets-manifest
+init_react();
+var assets_manifest_default = { "version": "227edd6e", "entry": { "module": "/build/entry.client-DDNEE3FT.js", "imports": ["/build/_shared/chunk-QN4KYTZP.js"] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "module": "/build/root-AQTRFCBY.js", "imports": void 0, "hasAction": false, "hasLoader": false, "hasCatchBoundary": false, "hasErrorBoundary": false }, "routes/$slug": { "id": "routes/$slug", "parentId": "root", "path": ":slug", "index": void 0, "caseSensitive": void 0, "module": "/build/routes/$slug-Z2EXBDGF.js", "imports": void 0, "hasAction": false, "hasLoader": true, "hasCatchBoundary": false, "hasErrorBoundary": true }, "routes/index": { "id": "routes/index", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "module": "/build/routes/index-76LOCTHF.js", "imports": void 0, "hasAction": false, "hasLoader": true, "hasCatchBoundary": false, "hasErrorBoundary": false } }, "url": "/build/manifest-227EDD6E.js" };
+
+// server-entry-module:@remix-run/dev/server-build
+var entry = { module: entry_server_exports };
+var routes = {
+  "root": {
+    id: "root",
+    parentId: void 0,
+    path: "",
+    index: void 0,
+    caseSensitive: void 0,
+    module: root_exports
+  },
+  "routes/$slug": {
+    id: "routes/$slug",
+    parentId: "root",
+    path: ":slug",
+    index: void 0,
+    caseSensitive: void 0,
+    module: slug_exports
+  },
+  "routes/index": {
+    id: "routes/index",
+    parentId: "root",
+    path: void 0,
+    index: true,
+    caseSensitive: void 0,
+    module: routes_exports
+  }
+};
+
+// server.js
+var server_default = (0, import_vercel.createRequestHandler)({ build: server_build_exports, mode: "production" });
+module.exports = __toCommonJS(server_exports);
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {});
+/**
+ * @remix-run/node v1.2.3
+ *
+ * Copyright (c) Remix Software Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */
+/**
+ * @remix-run/react v1.2.3
+ *
+ * Copyright (c) Remix Software Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */
+/**
+ * @remix-run/server-runtime v1.2.3
+ *
+ * Copyright (c) Remix Software Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */
