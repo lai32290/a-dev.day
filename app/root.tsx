5be60b64ed38e11d92b4499342052bb03c3bcec4
Lai <lai32690@gmail.com>
2022-03-14 17:46:07 -0300

    add google analytics

--- app/root.tsx
+++ app/root.tsx
@@ -28,6 +28,24 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MHDS3DKFYB"></script>
+
+        <script dangerouslySetInnerHTML={{__html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MHDS3DKFYB');
+          `}} />
+        <script dangerouslySetInnerHTML={{__html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TZ4NBXX');
+          `}} />
         <Meta />
         <Links />
       </head>
@@ -36,6 +54,11 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZ4NBXX"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
       </body>
     </html>
   );
